11bf8b8ed622130326f3e8463e1d6baad5f6ff26
furstinvia <furstinaptr@gmail.com>
2025-03-19 23:02:41 +0700

    happybirthday

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Black // background hitam
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday Furstin!",
+                        from = "From Gayu",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            color = Color.White, // warna teks  putih
+            fontFamily = FontFamily.Cursive // font
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            color = Color.White, //  warna teks  putih
+            fontFamily = FontFamily.Monospace, //  font  pengirim
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        Surface(color = Color.Black) { //  preview juga hitam
+            GreetingText(message = "Happy Birthday Furstin!", from = "From Gayu")
+        }
+    }
+}
